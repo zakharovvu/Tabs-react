099da6b2eb886d91aa0a3ad6739475c0dd4b2288
Zakharov Viacheslav <zakharovvu@gmail.com>
2019-02-24 19:30:41 +0200

    Updates

---
+++ precache-manifest.68bbc7a44ea1369ed4cca3c01b7016b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da6735d09649bce79fc9",
+    "url": "/Tabs-react/static/css/main.2203d45a.chunk.css"
+  },
+  {
+    "revision": "da6735d09649bce79fc9",
+    "url": "/Tabs-react/static/js/main.da6735d0.chunk.js"
+  },
+  {
+    "revision": "d0f69e97687f986f96b8",
+    "url": "/Tabs-react/static/js/runtime~main.d0f69e97.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/Tabs-react/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "8a53a5bfd88117762888d5ae33b48beb",
+    "url": "/Tabs-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Tabs-react/precache-manifest.f0955e21398aae7fedb6b78ded400fce.js"
+  "/Tabs-react/precache-manifest.68bbc7a44ea1369ed4cca3c01b7016b4.js"
 );
 
 workbox.clientsClaim();
