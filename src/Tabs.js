89be92e35ff56a3d2202e0aa055774a16d5be045
Zakharov Viacheslav <zakharovvu@gmail.com>
2019-02-24 19:34:10 +0200

    dev

---
+++ src/Tabs.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+    const arrTabs = props.children.map(el => el.props);
+
+    this.state = {
+      value: arrTabs,
+      taba: arrTabs[0].children,
+      tab: arrTabs[0].title
+    };
+  }
+
+  viwer(val) {
+    let tab = this.state.value.find(tab => tab.title === val);
+    
+    this.setState({ taba: tab.children, tab: val });
+  }
+
+  render() {
+    return (
+      <div className="Tabs">
+        <div className="Contenttab">
+          {this.state.value.map((el, index) => {
+            return (
+              <div
+                className={
+                  this.state.value[index].title === this.state.tab
+                    ? "Tab Tec"
+                    : "Tab"
+                }
+                key={index}
+                onClick={() => this.viwer(this.state.value[index].title)}
+              >
+                {el.title}
+              </div>
+            );
+          })}
+        </div>
+        <div className="Content">{this.state.taba}</div>
+      </div>
+    );
+  }
+}
+
+export default Tabs;
